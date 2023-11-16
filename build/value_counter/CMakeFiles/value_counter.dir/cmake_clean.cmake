24597d5d16a2bb93a11f7f02ecda2a688cec633e
root <root@ennnnasi>
2023-11-16 09:58:08 +0000

    adding  to CMakeLists.txt and built the pkg

---
+++ build/value_counter/CMakeFiles/value_counter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/value_counter.dir/src/value_counter.cpp.o"
+  "CMakeFiles/value_counter.dir/src/value_counter.cpp.o.d"
+  "value_counter"
+  "value_counter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/value_counter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
